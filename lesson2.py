ac64193a515d87029c9de83d73374134107c4a75
Samorae <samorae@wlhs.wlwv.k12.or.us>
2025-02-04 09:26:03 -0800

    update

---
+++ lesson2.py
@@ -0,0 +1,17 @@
+def find_average_price(price1,price2):
+    return (price1+price2/2)
+def cube_volume(volume):
+    return sidelength**3
+def main():
+    """price1 = float(input("What is the price? "))
+    price2 = float(input('What is the next price? '))
+    avg_price = find_average_price(price1, price2)
+    print(f"The average price of {price1 and {price2} is {avg_price}}")
+    """
+
+
+
+
+
+if __name__ == '__main__':
+    main()
