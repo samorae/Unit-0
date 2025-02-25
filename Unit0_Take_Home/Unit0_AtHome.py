73bb91d75f0a94f0b6fbb3d25b587eb1bafe6a8e
Eli Samora <samorae@wlhs.wlwv.k12.or.us>
2025-02-25 09:26:54 -0800

    Hello

---
+++ Unit0_Take_Home/Unit0_AtHome.py
@@ -0,0 +1,14 @@
+province = {
+    'A':'Newfoundland', 
+    'B': 'Nova Scotia', 
+    'C': 'Prince Edward Island',
+    'E': 'New Brunswick',
+    'G'or'H'or'J': 'Quebec',
+    'K'or'L'or'M'or'N'or'P':'Ontario',
+    'R': 'Manitoba',
+    'S': 'Saskatchewan',
+    'T': 'Alberta',
+    'V': 'British Columbia',
+    'X': 'Nunavut'or'Northwest Territories', 
+    'Y': 'Yukon' }
+print('HEllwo')
