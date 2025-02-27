d321c661b21e4e7b350d714d3aefa4ea1a76c589
Eli Samora <samorae@wlhs.wlwv.k12.or.us>
2025-02-27 08:47:45 -0800

    update

--- Unit0_Take_Home/Unit0_AtHome.py
+++ Unit0_Take_Home/Unit0_AtHome.py
@@ -1,43 +1,36 @@
 province = {
-    'A':'Newfoundland', 
-    'B': 'Nova Scotia', 
+    'A': 'Newfoundland',
+    'B': 'Nova Scotia',
     'C': 'Prince Edward Island',
     'E': 'New Brunswick',
-    'Quebec': 'G' or 'H' or 'J',
-    'Ontario':'K'or'L'or'M'or'N'or'P',
+    'G': 'Quebec','H': 'Quebec','J':'Quebec',
+    'K':'Ontario','L': 'Ontario', 'M':'Ontario','N':'Ontario','P':'Ontario',
     'R': 'Manitoba',
     'S': 'Saskatchewan',
     'T': 'Alberta',
     'V': 'British Columbia',
-    'X': 'Nunavut'or'Northwest Territories', 
-    'Y': 'Yukon' } 
-user_code = input('What is your poastal code?: ').upper()
+    'X': 'Nunavut/Northwest Territories',
+    'Y': 'Yukon' }
 
-if user_code[0] == 'A':
-    print(f'Your poastal code is {province["A"]}')
-elif user_code[0] == 'B':
-    print(f'Your poastal code is {province["B"]}')
-elif user_code[0] == 'C':
-    print(f'Your poastal code is {province["C"]}')
-elif user_code[0] == 'E':
-    print(f'Your poastal code is {province["E"]}')
-elif user_code[0] == 'G' or 'H' or 'J':
-    province = 'Quebec'
-    print(f'Your poastal code is {province}')
-elif user_code[0] == 'K' or 'L' or 'M' or 'N' or 'P':
-    province = "Ontario"
-    print(f'Your poastal code is {province}')
-elif user_code[0] == 'R':
-    print(f'Your poastal code is {province["R"]}')
-elif user_code[0] == 'S':
-    print(f'Your poastal code is {province["S"]}')
-elif user_code[0] == 'T':
-    print(f'Your poastal code is {province["T"]}')
-elif user_code[0] == 'V':
-    print(f'Your poastal code is {province["V"]}')
-elif user_code[0] == 'X':
-    print(f'Your poastal code is {province["X"]}')
-elif user_code[0] == 'Y':
-    print(f'Your poastal code is {province["Y"]}')
-else: 
-    print('Invalid poastal code')
+while True:
+    user_code = input('Type your poastal code in the following format(*** ***): ').upper()
+    if len(user_code) != 7:
+        print("Invalid postal code format.")
+        continue
+   
+    else:
+        first = user_code[0]
+        second = user_code[1]
+
+        if first in province:
+            location_type = second
+            if location_type == '0':
+                location_type = 'Rural'
+            else:
+                location_type = 'Urban'
+            print(f'You typed {user_code.upper()}')
+            print(f'The poastal code is a(n) {location_type} location, in {province[first]}')
+            break
+        else:
+            print('There is no place with that poastal code')
+            break
